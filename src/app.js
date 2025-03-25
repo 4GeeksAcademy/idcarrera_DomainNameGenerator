c46dd523730300137efc9f6464bc1722decf8d81
Danahe <danaheqb@gmail.com>
2025-03-25 00:40:03 +0000

    domainGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+// let pronoun = ['the', 'our', 'they'];
+// let adj = ['fabulos', 'amazing', 'kawaii'];
+// let noun = ['chicken', 'racoon', 'capybara'];
+
+let arrayDomain = [
+   ['the', 'our', 'they'],
+   ['fabulos', 'amazing', 'kawaii'],
+   ['chicken', 'racoon', 'capybara']
+];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for (let i = 0; i < arrayDomain.length; i++) {
+    //console.log(arrayDomain[0][i])
+    for (let j = 0; j < arrayDomain.length; j++) {
+        //console.log(arrayDomain[0][i] + arrayDomain[1][j])
+        for (let k = 0; k < arrayDomain.length; k++){
+            //console.log(arrayDomain[i] + arrayDomain [j] + arrayDomain[k])
+            console.log(arrayDomain[0][i] + arrayDomain [1][j] + arrayDomain [2][k] + ".com") 
+        }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    }
+        //newDomain = pronoun[i] + adj [i] + noun [i]
+}
+
+
+
+
